97f9515d63c013173af74d14e3138c31a8918160
Drusti Patel <drustipatel@Drustis-MacBook-Pro.local>
2022-06-08 21:16:07 -0400

    welcome page just worked

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
+import PublicRoute from './utils/PublicRoute';
 import {
   ApolloClient,
   InMemoryCache,
@@ -6,15 +9,16 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //IMPORT PAGES NEXT
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -30,6 +34,7 @@ const httpLink = createHttpLink({
       headers: {
         ...headers,
         authorization: token ? `Bearer ${token}` : '',
+
       },
     };
   });
@@ -41,6 +46,27 @@ const client = new ApolloClient({
     cache: new InMemoryCache(),
   });
 
-//   //function App() {
-//       return
-//   }
+  function App() {
+      return (
+        <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/login">Login</NavLink><small></small>
+            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink><small></small>
+          </div>
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<Home/>} />
+              <Route path="/login" component={Login} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+      );
+  }
+
+  export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React from 'react';
+const quotes = require('./quotes.json');
 function Home() {
+
   return (
-    <div>
-      <p className="content is-medium, is italic">Welcome to Beer Happy!</p>
-      <hr/>
-      <img className="my-pic" src={process.env.PUBLIC_URL + Home } alt="Home Image"/>
-      <p className="content">
-      The start of the pandemic brought about many changes to the food service industry. Among those changes is the increase in "home-brew" mixologists! To help handle the sudden growth and interest in mocktails and cocktails, we introduce Beer Happy! Beer Happy is an app designed for the home-brew mixologist with an easy to use interface that allows users to research any drink they'd like to try making, and save ones they enjoy to their profile! Our five-star rating system allows users to see which cocktails are the most popular. 
-      </p>
+    <div class="home">
+      Welcome to the Beer Happy!
+     
+    
+    <h5> Quotes: <span className="auto-type"></span> </h5>
+
     </div>
+
   );
 }
+
 
 export default Home;
