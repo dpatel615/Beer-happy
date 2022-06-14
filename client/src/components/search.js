72ec0b2bf880d8059eeaf9a57199721b847c781e
Drusti Patel <drustipatel@Drustis-MacBook-Pro.local>
2022-06-13 21:13:18 -0400

    changes

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,41 +1,11 @@
 import { renderToStringWithData } from '@apollo/client/react/ssr';
 import React from 'react';
 import '../index.css';
-import Axios from "axios";
-import { useState } from "react";
-import DrinksBox from './strdrinks';
 
 
 function SearchBar() {
 
- // const [query, setquery] = useState("")
-  const [drinks, setdrinks] = useState([])
-  const filterCategory = {
-    method: 'GET',
-    url: 'https://the-cocktail-db.p.rapidapi.com/filter.php',
-    params: { c: 'Cocktail' },
-    headers: {
-      'X-RapidAPI-Key': '3d40ed704fmsh792fc0b1a725724p1e139cjsn1573a9112c0a',
-      'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
-    }
-  };
- // const drinkstr = [0];
-   //const isArray = Object.prototype.toString.call(drinkstr) === '[object Array]';
-  const drinkSearch = (event) => {
-    event.preventDefault();
-  Axios.request(filterCategory).then(function (response) {
-    // console.log(drinks);
-    console.log(response.data.drinks);
-     setdrinks(response.data.drinks);
-    //console.log(drinks);
-    
-  }).catch(function (error) {
-    console.error(error);
-   
-   // filterCategory();
-  });
-}
-
+ 
   return (
     <div id="search">
       <form action="/" method="get">
@@ -50,23 +20,15 @@ function SearchBar() {
           name="s"
         //  onChange={(e) => setquery(e.target.value)}
         />
-        <button onClick={drinkSearch}>Search</button>
-      </form>
-      <div>
-        
-        {drinks.map((drinks) => {
-          return <DrinksBox key={drinks} drinks={drinks}/>
-        })}
-         {/* <p>{drinks.strDrink[0]}</p> */}
-         {/* drinks[0].strDrink; */}
-      </div>
+        <button>Search</button>
+       </form>
+       
+      
     </div>
   )
-
   
-
 }
-export default SearchBar;
+ export default SearchBar;
 
 //const filterCategory = {
 //   params: {c: 'Cocktail'},
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { getUser, removeUserSession } from '../utils/Common';
+import Axios from "axios";
+import { useState } from "react";
+import DrinksBox from '../components/strdrinks';
+
 
 function Dashboard(props) {
   // const user = getUser();
@@ -7,7 +11,35 @@ function Dashboard(props) {
   // useEffect(()=> {
 
   // })
+ // const [query, setquery] = useState("")
+ const [drinks, setdrinks] = useState([])
+ const filterCategory = {
+   method: 'GET',
+   url: 'https://the-cocktail-db.p.rapidapi.com/filter.php',
+   params: { c: 'Cocktail' },
+   headers: {
+     'X-RapidAPI-Key': '3d40ed704fmsh792fc0b1a725724p1e139cjsn1573a9112c0a',
+     'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+   }
+ };
+// const drinkstr = [0];
+   //const isArray = Object.prototype.toString.call(drinkstr) === '[object Array]';
+ const drinkSearch = (event) => {
+   event.preventDefault();
+ Axios.request(filterCategory).then(function (response) {
+   //console.log(drinks);
+   console.log(response.data.drinks);
+    setdrinks(response.data.drinks);
+   //console.log(drinks);
+   
+ }).catch(function (error) {
+   console.error(error);
+  
+   //filterCategory();
+  drinkSearch();
+ });
 
+ }
   // handle click event of logout button
   const handleLogout = () => {
     removeUserSession();
@@ -20,8 +52,17 @@ function Dashboard(props) {
       <h1>Hi!</h1>
       <img className="dashimg" src={require(`../Assets/${imgArr[Math.floor(Math.random()*imgArr.length)]}.jpg`).default}></img>
       <input type="button" onClick={handleLogout} value="Logout" />
+      
+      <button onClick={drinkSearch}></button>
+
+        {drinks.map((drinks) => {
+       return <DrinksBox drinks={drinks}/>
+        })}
+
     </div>
+  
+
   );
-}
+      }
 
 export default Dashboard;
